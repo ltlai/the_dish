94738e95cd6526354d999cd6184fa3c0e10a8de2
ltlai <leslie.t.lai@gmail.com>
2014-11-11 10:57:00 -0800

    Create new Rails project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+TheDish::Application.config.secret_key_base = 'db59c9a299c5a396580fa25e7565f69c95a14e7899af699e8298e4c99254b5eae53572a03a8babc681b736db74fd571c6033b7672d7ea0ec536a911fa6be184b'
