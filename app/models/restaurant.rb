1aceca40e47fdad6308837f225648b0d9e722d74
ltlai <leslie.t.lai@gmail.com>
2014-11-11 13:11:22 -0800

    Calculate and display restaurant score

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -2,4 +2,16 @@ class Restaurant < ActiveRecord::Base
   def display_address
     "#{street_address}, #{city}, #{state}"
   end
+
+  def score
+    if vote_count == 0
+      return "No score yet..."
+    else
+      "#{(fresh_votes.to_f / vote_count * 100).round}%"
+    end
+  end
+
+  def vote_count
+    fresh_votes + rotten_votes
+  end
 end
